98bca56718af6463c28439c43130f7b69c68edaf
Mischa Dhar <mischadhar94@gmail.com>
2024-04-19 16:23:51 +0100

    created first interation of basical terminal app

--- data_proc/data_proc.py
+++ data_proc/data_proc.py
@@ -8,5 +8,10 @@ def tfl_modes(mode_api_data):
 def status_proc(status_data):
 
     status = status_data[0]['lineStatuses'][0]['statusSeverityDescription']
+    status_code = status_data[0]['lineStatuses'][0]['statusSeverity']
 
-    return status
+    return status, status_code
+
+def issue_reason(status_data):
+
+    return status_data[0]['lineStatuses'][0]['reason']
--- main/interface.py
+++ main/interface.py
@@ -1,19 +1,31 @@
 import survey
 
-from data_proc.data_proc import tfl_modes, status_proc
+from data_proc.data_proc import issue_reason, tfl_modes, status_proc
 from status_api.status import line_status, return_modes
 from params import API_KEY
 
 if __name__ == "__main__":
 
-    modes = tfl_modes(return_modes(app_key=API_KEY))
+    modes_data = return_modes(app_key=API_KEY)
+    modes = tfl_modes(modes_data)
 
     mode_idx = survey.routines.select('Please select a TFL service: '
                                    , options = modes)
 
     chosen_mode = modes[mode_idx]
 
-    line_status = status_proc(line_status(app_key=API_KEY
-                                          , modes=chosen_mode))
+    line_data = line_status(app_key=API_KEY, modes=chosen_mode)
+    line_status, line_status_code = status_proc(line_data)
 
-    print(f"\nThere is {line_status} on the {chosen_mode}")
+    if line_status_code == 10:
+
+        print(f"\nThere is {line_status} on the {chosen_mode}")
+
+    else:
+
+        reason = issue_reason(line_data).split(':')[1]
+
+        print('Bad luck friend')
+        print(f"The {chosen_mode} is experiencing a {line_status.lower()}")
+        print(f'The reason is:')
+        print(reason.strip())
