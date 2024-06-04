1a0079bd9cd17fd14546a0cc3db221e10f5bf2bb
Mischa Dhar <mischadhar94@gmail.com>
2024-06-04 16:03:05 +0100

    updated cli package to properly reflect tube line statuses, need to clean up

--- data_proc/data_proc.py
+++ data_proc/data_proc.py
@@ -20,9 +20,34 @@ def status_proc(status_data):
 
     return status.lower(), status_code
 
+def status_proc_tube(status_data):
+
+    """Given the data from the TFL line API, returns the status of a 'mode'
+    alongside the numeric code for that status - TUBE SPECIFIC"""
+
+    tube_dict = {'line':[],'status':[],'status_code':[]}
+
+    for x in status_data:
+        tube_dict['line'].append(x['id'])
+        tube_dict['status'].append(x['lineStatuses'][0]['statusSeverityDescription'])
+        tube_dict['status_code'].append(x['lineStatuses'][0]['statusSeverity'])
+
+    # breakpoint()
+
+    return tube_dict
+
 def issue_reason(status_data):
 
     """Returns the description of why a 'mode' is being impacted
     by an issue"""
 
     return status_data[0]['lineStatuses'][0]['reason']
+
+def issue_reason_tube(status_data):
+
+    """Returns the description of why a 'mode' is being impacted
+    by an issue"""
+
+    # breakpoint()
+
+    return status_data['lineStatuses'][0]['reason']
